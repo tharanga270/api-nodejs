ac93be1558fe09e66fdee49c77518ded6c89ba1e
Isuru <devloperwordpress270@gmail.com>
2023-11-06 18:38:40 +0530

    2

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -21,15 +21,57 @@ const saveCustomer = (req,resp)=>{
     });
 };
 const findCustomer = (req,resp)=>{
-    
+    Customer.findOne({nic:req.headers.nic}).then(result=>{
+        if(result==null){
+            resp.status(404).json({status:false, message:'customer not found!'});
+        }else{
+            resp.status(200).json({status:true, data:result});
+        }
+    }).catch(error=>{
+        resp.status(500).json(error);
+    });
 };
 const updateCustomer = (req,resp)=>{
-    
+    Customer.updateOne({nic:req.headers.nic},{
+        $set:{
+            name:req.body.name,
+            address:req.body.address,
+            salary:req.body.salary
+        }
+    }).then(result=>{
+        if(result.nModified>0){
+            resp.status(201).json({status:true, message:'customer was Updated!'});
+        }else{
+            resp.status(200).json({status:false, message:'try again!'});
+        }
+    }).catch(error=>{
+        resp.status(500).json(error);
+    });
 };
 
 const deleteCustomer = (req,resp)=>{
-    
+    Customer.deleteOne({nic:req.headers.nic}).then(result=>{
+        if(result.deletedCount>0){
+            resp.status(204).json({status:true, message:'customer was Deleted!'});
+        }else{
+            resp.status(400).json({status:false, message:'try again'});
+        }
+    }).catch(error=>{
+        resp.status(500).json(error);
+    });
 };
 const findAllCustomers = (req,resp)=>{
-    
+    Customer.find().then(result=>{
+        resp.status(200).json({status:true, data:result});
+    }).catch(error=>{
+        resp.status(500).json(error);
+    });
 };
+
+module.exports={
+    saveCustomer,
+    findCustomer,
+    updateCustomer,
+    deleteCustomer,
+    findAllCustomers
+}
